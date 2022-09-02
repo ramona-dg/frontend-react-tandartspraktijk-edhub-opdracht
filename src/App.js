74ab37bce3340739ca170a295994029b67f630f5
ramona-dg <ramona.rdg@gmail.com>
2022-09-02 10:00:22 +0200

    active navlinks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import TopMenu from './components/TopMenu';
 import HomePage from './pages/Home';
 import CavitiesPage from './pages/Cavities';
@@ -7,15 +8,26 @@ import WhiteningPage from './pages/Whitening';
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <TopMenu />
-      <HomePage />
-      <CavitiesPage />
-      <AppointmentsPage />
-      <WhiteningPage />
-    </>
-  );
+    return (
+        <Router>
+            <TopMenu/>
+
+            <Switch>
+                <Route exact path="/">
+                    <HomePage/>
+                </Route>
+                <Route path="/gaatjes">
+                    <CavitiesPage/>
+                </Route>
+                <Route path="/afspraak-maken">
+                    <AppointmentsPage/>
+                </Route>
+                <Route path="/tanden-bleken">
+                    <WhiteningPage/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
